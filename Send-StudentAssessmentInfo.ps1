1eec659f8c5b73bdb0f0ab621ce9a0f38e760679
Justin Cooper <jcooper@chicousd.org>
2025-04-07 08:10:19 -0700

    prior hel

--- Send-StudentAssessmentInfo.ps1
+++ Send-StudentAssessmentInfo.ps1
@@ -1,120 +1,107 @@
 [cmdletbinding()]
 param (
-    [Parameter(Mandatory = $True)]
-    [string]$SISServer,
-    [Parameter(Mandatory = $True)]
-    [string]$SISDatabase,
-    [Parameter(Mandatory = $True)]
-    [System.Management.Automation.PSCredential]$SISCredential,
-    [Parameter(Mandatory = $True)]
-    [System.Management.Automation.PSCredential]$EmailCredential,
-    [Parameter(Mandatory = $false)]
-    [string[]]$Bcc,
-    [Alias('wi')]
-    [switch]$WhatIf
+ [Parameter(Mandatory = $True)]
+ [string]$SISServer,
+ [Parameter(Mandatory = $True)]
+ [string]$SISDatabase,
+ [Parameter(Mandatory = $True)]
+ [System.Management.Automation.PSCredential]$SISCredential,
+ [Parameter(Mandatory = $True)]
+ [System.Management.Automation.PSCredential]$EmailCredential,
+ [Parameter(Mandatory = $false)]
+ [string[]]$Bcc,
+ [Alias('wi')]
+ [switch]$WhatIf
 )
 
-function Get-Priors ($csvFile) {
-    process {
-        Write-Host ('{0},{1}' -f $MyInvocation.MyCommand.Name, $csvFile) -F Green
-        if (!(Test-Path $csvFile)) { 'key' | Export-Csv -Path $csvFile }
-        Import-Csv $csvFile
-    }
+function Get-Priors ($priorDataFile) {
+ process {
+  Write-Host ('{0},{1}' -f $MyInvocation.MyCommand.Name, $priorDataFile) -F Green
+  if (!(Test-Path '.\data')) { New-Item -Path .\data -ItemType Directory }
+  if (!(Test-Path $priorDataFile)) { 'Prior Data' | Out-File -Path $priorDataFile }
+  Get-Content -Path $priorDataFile
+ }
 }
 
 function Format-Obj ($type) {
-    process {
-        $obj = '' | Select-Object dbRow, msg, subject, type, key
-        $leaveDate = Get-Date $_.'SC Leave Date' -F yyyyMMdd
-        $obj.key = $_.ID + $_.School + $leaveDate
-        $obj.dbRow = $_
-        $obj.type = $type
-        $obj
-    }
+ process {
+  $obj = '' | Select-Object dbRow, msg, subject, type, key
+  $leaveDate = Get-Date $_.'SC Leave Date' -F yyyyMMdd
+  $obj.key = $_.ID + $_.School + $leaveDate
+  $obj.dbRow = $_
+  $obj.type = $type
+  $obj
+ }
 }
 
 function Format-EmailMsg ($baseMsg) {
-    process {
-        $msg = $baseMsg -replace 'STU.ID', $_.dbRow.ID
-        $msg = $msg -replace 'STU.FN', $_.dbRow.FN
-        $msg = $msg -replace 'STU.LN', $_.dbRow.LN
-        $_.msg = $msg
-        $_
-    }
+ process {
+  $msg = $baseMsg -replace 'STU.ID', $_.dbRow.ID
+  $msg = $msg -replace 'STU.FN', $_.dbRow.FN
+  $msg = $msg -replace 'STU.LN', $_.dbRow.LN
+  $_.msg = $msg
+  $_
+ }
 }
 
 function Select-Latest ($data) {
-    begin {
-    }
-    process {
-        # Write-Host ('{0}' -f $MyInvocation.MyCommand.Name)
-        $uniqueIDs = ($data | Select-Object -Property ID | Sort-Object -Property ID -Unique).ID
-        Foreach ($id in $uniqueIDs) {
-            $allMatching = $data | Where-Object { $_.ID -eq $id }
-            # Write-Host ($allMatching | Out-String) -F Blue
-            $result = $allMatching | Sort-Object -Property ID, 'SC Leave Date' -Descending | Select-Object -First 1
-            # Write-Host ($result | Out-String) -F Green
-            # read-host wait
-            $result
-        }
-    }
+ begin {
+ }
+ process {
+  # Write-Host ('{0}' -f $MyInvocation.MyCommand.Name)
+  $uniqueIDs = ($data | Select-Object -Property ID | Sort-Object -Property ID -Unique).ID
+  Foreach ($id in $uniqueIDs) {
+   $allMatching = $data | Where-Object { $_.ID -eq $id }
+   # Write-Host ($allMatching | Out-String) -F Blue
+   $result = $allMatching | Sort-Object -Property ID, 'SC Leave Date' -Descending | Select-Object -First 1
+   # Write-Host ($result | Out-String) -F Green
+   # read-host wait
+   $result
+  }
+ }
 }
 
 function Send-Msg {
-    begin {
-        $mailParams = @{
-            From       = '<{0}>' -f $EmailCredential.Username
-            Subject    = $null
-            BodyAsHTML = $True
-            SMTPServer = 'smtp.office365.com'
-            Cred       = $EmailCredential # use a valid Office365 account with Flow rules in place to prevent SPAM warnings.
-            UseSSL     = $True
-            Port       = 587
-        }
-        if ( $Bcc ) { $mailParams.Bcc = $Bcc } # Add Bcc to outgoing email messages.
-    }
-    process {
-        $mailParams.Body = $_.msg
-        $mailParams.To = $_.dbRow.Email
-        $mailParams.Subject = 'Forward Assessment Info - ' + $_.dbRow.FN + ' ' + $_.dbRow.LN
-        Write-Verbose ($mailParams | Out-String)
-        $msg = $MyInvocation.MyCommand.Name, $mailParams.To,
-        $_.dbRow.ID, $_.dbRow.School , $_.type
-        Write-Host ('{0},{1},{2},{3},{4}' -f $msg) -F Blue
-        if (!$WhatIf) {
-            Try {
-                Write-Debug  'Send Mail Message?'
-                Send-MailMessage @mailParams
-            }
-            catch {
-                Write-Error ('{0},{1},{2}, ERROR sending email' -f $msg)
-                return
-            }
-        }
-        $global:skip += $_.key
-    }
+ begin {
+  $mailParams = @{
+   From       = '<{0}>' -f $EmailCredential.Username
+   Subject    = $null
+   BodyAsHTML = $True
+   SMTPServer = 'smtp.office365.com'
+   Cred       = $EmailCredential # use a valid Office365 account with Flow rules in place to prevent SPAM warnings.
+   UseSSL     = $True
+   Port       = 587
+  }
+  if ( $Bcc ) { $mailParams.Bcc = $Bcc } # Add Bcc to outgoing email messages.
+ }
+ process {
+  $mailParams.Body = $_.msg
+  $mailParams.To = $_.dbRow.Email
+  $mailParams.Subject = 'Forward Assessment Info - ' + $_.dbRow.FN + ' ' + $_.dbRow.LN
+  Write-Verbose ($mailParams | Out-String)
+  $msg = $MyInvocation.MyCommand.Name, $mailParams.To,
+  $_.dbRow.ID, $_.dbRow.School , $_.type
+  Write-Host ('{0},{1},{2},{3},{4}' -f $msg) -F Blue
+  if (!$WhatIf) { Send-MailMessage @mailParams }
+ }
 }
 
 function Skip-Priors ($priorData) {
-    begin {
-    }
-    process {
-        # No global skip here as it would prevent both admin and counselors from getting proper notifications
-        if ($priorData.key -contains $_.key) {
-            $msg = $MyInvocation.MyCommand.Name, ($_.dbRow.FN + ' ' + $_.dbRow.LN), $_.key
-            Write-Host ('{0},{1},{2}' -f $msg) -F Yellow
-            return
-        }
-        $_
-    }
+ process {
+  if ($priorData -contains $_.key) {
+   Write-Host ('{0},{1},{2}' -f $MyInvocation.MyCommand.Name, ($_.dbRow.FN + ' ' + $_.dbRow.LN), $_.key) -F DarkMagenta
+   return
+  }
+  $_
+ }
 }
 
-function Update-Priors ($csvFile, $priorData) {
-    process {
-        # if ($WhatIf -or ($priorData.key -contains $_.key) -or ($global:skip -contains $_.key)) { return }
-        Write-Host ('{0},{1}' -f $MyInvocation.MyCommand.Name, $_.key) -F Green
-        @{key = $_.key } | Export-Csv -Path $csvFile -Append -WhatIf:$WhatIf
-    }
+function Update-Priors ($file, $data) {
+ process {
+  if ($data -contains $_.key) { return }
+  Write-Host ('{0},{1}' -f $MyInvocation.MyCommand.Name, $_.key) -F Green
+  Add-Content -Path $file -Value $_.key -WhatIf:$WhatIf
+ }
 }
 
 # ==================== Main =====================
@@ -125,16 +112,13 @@ Import-Module -Name CommonScriptFunctions -Cmdlet $moduleCommands
 if ($WhatIf) { Show-TestRun }
 
 $sqlParamsSIS = @{
-    Server     = $SISServer
-    Database   = $SISDatabase
-    Credential = $SISCredential
+ Server     = $SISServer
+ Database   = $SISDatabase
+ Credential = $SISCredential
 }
 
-$priorCSV = '.\lib\priorNotifications.txt'
-if (!(Test-Path '.\lib')) { New-Item -Path .\lib -ItemType Directory }
-if (!(Test-Path $priorCSV)) { "key" | Out-File $priorCSV }
-$priorNotifications = Get-Priors $priorCsv
-$global:skip = @()
+$priorDataFile = '.\data\priorData.txt'
+$priorData = Get-Priors $priorDataFile
 
 # ======== ADMIN
 $adminSql = Get-Content .\sql\admin.sql -Raw
@@ -143,8 +127,7 @@ $adminData = New-SqlOperation @sqlParamsSIS -Query $adminSql | ConvertTo-Csv | C
 
 $adminMsg = Get-Content .\html\admin.html -Raw
 $adminObjs = Select-Latest $adminData | Format-Obj Admin
-$adminObjs | Skip-Priors $priorNotifications |
-Format-EmailMsg $adminMsg | Send-Msg
+$adminObjs | Skip-Priors $priorData | Format-EmailMsg $adminMsg | Send-Msg
 
 # ======== COUNSELORS
 $counselorSql = Get-Content .\sql\counselors.sql -Raw
@@ -153,10 +136,9 @@ $counselorData = New-SqlOperation @sqlParamsSIS -Query $counselorSql | ConvertTo
 
 $counselorMsg = Get-Content .\html\counselors.html -Raw
 $counselorObjs = Select-Latest $counselorData | Format-Obj Counselors
-$counselorObjs | Skip-Priors $priorNotifications |
-Format-EmailMsg $counselorMsg | Send-Msg
+$counselorObjs | Skip-Priors $priorData | Format-EmailMsg $counselorMsg | Send-Msg
 
-$counselorObjs | Update-Priors $priorCSV $priorNotifications
-$adminObjs | Update-Priors $priorCSV $priorNotifications
+$counselorObjs | Update-Priors $priorDataFile $priorData
+$adminObjs | Update-Priors $priorDataFile $priorData
 
 if ($WhatIf) { Show-TestRun }
